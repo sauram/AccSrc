c087633d5d002d282b7591d88fbdb9fc305d0ac8
Saurabh Singh Gautam <32809271+sauram@users.noreply.github.com>
2021-01-08 19:53:33 +0530

    Added comments

--- src/main/java/com/example/sensordata/MainActivity.java
+++ src/main/java/com/example/sensordata/MainActivity.java
@@ -18,11 +18,6 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * SensorListeners demonstrates how to gain access to sensors (here, the light
- * and proximity sensors), how to register sensor listeners, and how to
- * handle sensor events.
- */
 public class MainActivity extends AppCompatActivity
         implements SensorEventListener {
 
@@ -65,9 +60,7 @@ public class MainActivity extends AppCompatActivity
         mSensorManager = (SensorManager) getSystemService(
                 Context.SENSOR_SERVICE);
 
-        // Get light and proximity sensors from the sensor manager.
-        // The getDefaultSensor() method returns null if the sensor
-        // is not available on the device.
+        
         mSensorAcc = mSensorManager.getDefaultSensor(
                 Sensor.TYPE_ACCELEROMETER);
 
